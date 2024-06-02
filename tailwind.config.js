070894fa9da5d101b405bfe7fe9ec9f2454d6be7
flexedosp <farinoer13@gmail.com>
2024-06-02 10:05:23 +0700

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+// This config cannot be use if you not delete inside index.css
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    // eslint-disable-next-line no-undef
+    require('daisyui'),
+  ],
+}
